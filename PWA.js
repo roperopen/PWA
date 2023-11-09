27f7d2ec5f33ce4f7769809a257579fc3349f574
Ropen Kapsuzian <120184160+roperopen@users.noreply.github.com>
2023-11-09 12:49:08 +0100

    modified service worker

--- PWA.js
+++ PWA.js
@@ -21,4 +21,18 @@ if ("serviceWorker" in navigator) {
   } else {
     console.error("Service workers are not supported.");
   }
-  
+  self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('v1')
+            .then(cache => {
+                cache.addAll([
+                    './',
+                    './style.css',
+                    './sw.js'
+                ]);
+                console.log('Assets cached')
+                
+            })
+            .catch(err => console.log('could not cache assets')) 
+    )  
+})
